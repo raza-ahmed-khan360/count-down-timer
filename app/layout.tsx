22ea3c015fd723663604886ea28a5fb41b6cc9bc
Raza Ahmed Khan <razaahmedkhan360@gmail.com>
2024-11-22 15:58:13 +0500

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-plus-jakarta-sans",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Countdown App",
+  description: "Created with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${plusJakartaSans.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Countdown from '@/components/countdown'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='font-sans'>
+      <Countdown/>
+    </div>
+  )
+}
+
+export default Home
